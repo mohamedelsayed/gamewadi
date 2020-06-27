2ca93db6eaf5fb60dce0e90af6b8a8455744d214
Mohamed Elsayed <me@mohamedelsayed.net>
2020-06-27 02:38:38 +0200

    fixPermissions.sh php-extensions.sh files

---
+++ fixPermissions.sh
@@ -0,0 +1,12 @@
+sudo mkdir -p storage/framework
+sudo mkdir -p storage/framework/sessions
+sudo mkdir -p storage/framework/views
+sudo mkdir -p storage/framework/cache
+sudo mkdir -p storage/logs
+sudo mkdir -p bootstrap/cache
+sudo mkdir -p public/zip
+sudo chmod 777 -R storage/framework
+sudo chmod 777 -R storage/logs
+sudo chmod 777 -R bootstrap/cache
+sudo chmod 777 -R public/zip
+sudo chmod 777 -R public/error-output.txt
