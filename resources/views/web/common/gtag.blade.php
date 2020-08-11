b417c898959c6fa9d55be7d47c939ff1b31589b3
Mohamed Elsayed <me@mohamedelsayed.net>
2020-08-12 01:11:26 +0200

    add google analytics

---
+++ resources/views/web/common/gtag.blade.php
@@ -0,0 +1,11 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-171632706-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-171632706-1');
+</script>
