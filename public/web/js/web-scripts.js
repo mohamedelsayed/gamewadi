34dca7dab57f9994253e546fdec8f9ac8d2c760b
Mohamed Elsayed <me@mohamedelsayed.net>
2020-11-21 05:10:14 +0200

    select first product

--- public/web/js/web-scripts.js
+++ public/web/js/web-scripts.js
@@ -4,6 +4,20 @@ $(document).ready(function () {
 //        console.log(country_id);
         showDenominationsByCountryId(country_id);
     }
+    $('body').on('click', '.qty-decrease', function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 0 ? 0 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('body').on('click', '.qty-increase', function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
 });
 function draw_denomination_table_web(selectObject) {
     var country_id = selectObject.value;
@@ -25,7 +39,9 @@ function showDenominationsByCountryId(country_id) {
 //    +'<br><small style="color:#A7578F; text-decoration:line-through;"> $ 11.000</small>'
                     + '</td>'
                     + '<td><div class="qty-control">'
-                    + '<i class="qty-decrease">-</i><input type="text" onkeypress="validate(event)" name="quantity[]" placeholder="0" value="0" data-qty="" data-price="10.000"><i class="qty-increase">+</i><i class="max-qty" data-max-qty="100"></i>'
+                    + '<i class="qty-decrease">-</i>'
+                    + '<input type="text" onkeypress="validate(event)" name="quantity[]" placeholder="0" value="0" data-qty="" data-price="10.000">'
+                    + '<i class="qty-increase">+</i><i class="max-qty" data-max-qty="100"></i>'
                     + '</div><input type="hidden" name="price[]" value="10.000">'
                     + '<input type="hidden" name="counter[]" value="100">'
                     + '<input type="hidden" name="denoId[]" value="71">'
